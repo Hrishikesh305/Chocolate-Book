5661465a3c125a801471805a04a1ad7c66e3eab8
Hrishikesh305 <55622810+Hrishikesh305@users.noreply.github.com>
2021-12-30 15:01:01 +0530

    Now Images in the buttons turn grey with hover

---
+++ Home Page/hoverManager.js
@@ -0,0 +1,8 @@
+$('#F')
+    .mouseenter(()=>(document.getElementById('Filterimg').src = 'Images\\Filter_grey.png'))
+    .mouseleave(()=>(document.getElementById('Filterimg').src = 'Images\\Filter.png'))
+    
+$('#S')
+    .mouseenter(()=>(document.getElementById('Spreadsheetimg').src = 'Images\\Spreadsheet_grey.png'))
+    .mouseleave(()=>(document.getElementById('Spreadsheetimg').src = 'Images\\Spreadsheet.png'))
+
